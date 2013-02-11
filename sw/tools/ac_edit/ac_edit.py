c300e5fbcfd439c15d0c99c9821f930aba807860
Christophe De Wagter <dewagter@gmail.com>
2013-02-11 21:27:52 +0100

    TreeView

--- sw/tools/ac_edit/ac_edit.py
+++ sw/tools/ac_edit/ac_edit.py
@@ -17,23 +17,39 @@ paparazzi_home = getenv("PAPARAZZI_HOME", path.normpath(path.join(
                         path.dirname(path.abspath(__file__)),
                         '../../../')))
 
-class Base:
-    def process(self, widget):
-        print("Button Process")
-        list_of_firmwares = glob.glob(path.join(paparazzi_home, "conf/firmwares/*.makefile"))
-        for firm in list_of_firmwares:
-            print(firm.replace(".makefile", "").replace(path.join(paparazzi_home, "conf/firmwares/"), ""))
-        for firm in list_of_firmwares:
-            self.combo.append_text(firm.replace(".makefile", "").replace(path.join(paparazzi_home, "conf/firmwares/"), ""))
+PAPARAZZI_FIRMWARES = path.join(paparazzi_home, "conf/firmwares/")
+PAPARAZZI_MODULES = path.join(paparazzi_home, "conf/modules/")
 
+
+class Base:
+
+    def load_airframe_xml(self):
         try:
-            tree = etree.parse(path.join(paparazzi_home, "conf/airframes/CDW/classix.xml"))
-            root = tree.getroot()
-            self.label1.set_text(root.tag)
+            self.tree = etree.parse(path.join(paparazzi_home, "conf/airframes/CDW/classix.xml"))
+            root = self.tree.getroot()
+            for block in root:
+                print block.tag
         except (IOError, etree.XMLSyntaxError) :
             raise
             self.error()
-        # elem = tree.getroot()
+   
+
+    def find_firmwares(self, widget):
+        list_of_firmwares = glob.glob( PAPARAZZI_FIRMWARES + "*.makefile" )
+        list_of_firmwares.sort()
+        self.combo.get_model().clear();
+        for firm in list_of_firmwares:
+            self.combo.append_text( firm.replace(".makefile","").replace(PAPARAZZI_FIRMWARES, "") )
+
+    def find_modules(self, widget):
+        list_of_modules = glob.glob( PAPARAZZI_MODULES + "*.xml" )
+        list_of_modules.sort();
+        self.combo.get_model().clear();
+        for mod in list_of_modules:
+            self.combo.append_text( mod.replace(".xml","").replace(PAPARAZZI_MODULES, "") )
+
+    def process(self, widget):
+        print etree.tostring(self.tree, pretty_print=True)
 
     def combo_changed(self, widget):
         print("Changed Combo")
@@ -56,10 +72,56 @@ class Base:
 
     def error(self):
         err_msg = gtk.MessageDialog(None, gtk.DIALOG_DESTROY_WITH_PARENT,
-                                    gtk.MESSAGE_ERROR, gtk.BUTTONS_CLOSE,
-                                    "Error Loading XML")
+            gtk.MESSAGE_ERROR, gtk.BUTTONS_CLOSE,
+            "Error Loading XML" )
         err_msg.run()
         err_msg.destroy()
+
+    def fill_tree_from_airframe(self):
+        
+        # create a TreeStore with one string column to use as the model
+        self.treestore = gtk.TreeStore(str)
+
+        # Load the File
+        try:
+            root = self.tree.getroot()
+            for block in root:
+                piter = self.treestore.append(None, [ block.tag ])
+                for att in block.attrib:
+                    self.treestore.append(piter, [ att ])
+
+        except (IOError, etree.XMLSyntaxError) :
+            self.error()
+
+
+        # create the TreeView using treestore
+        self.treeview = gtk.TreeView(self.treestore)
+
+        # create the TreeViewColumn to display the data
+        self.tvcolumn = gtk.TreeViewColumn('Airframe.xml')
+
+        # add tvcolumn to treeview
+        self.treeview.append_column(self.tvcolumn)
+
+        # create a CellRendererText to render the data
+        self.cell = gtk.CellRendererText()
+
+        # add the cell to the tvcolumn and allow it to expand
+        self.tvcolumn.pack_start(self.cell, True)
+
+        # set the cell "text" attribute to column 0 - retrieve text
+        # from that column in treestore
+        self.tvcolumn.add_attribute(self.cell, 'text', 0)
+
+        # make it searchable
+        self.treeview.set_search_column(0)
+
+        # Allow sorting on the column
+        # self.tvcolumn.set_sort_column_id(0)
+
+        # Allow drag and drop reordering of rows
+        # self.treeview.set_reorderable(True)
+        
 
     def destroy(self, widget, data=None):
         print("You clicked close")
@@ -70,20 +132,41 @@ class Base:
         # self.window.set_size_request(800,600)
         self.window.set_title("Paparazzi Airframe File Editor")
 
+        self.box1 = gtk.VBox()
+
+        ##### Buttons
         self.btnExit = gtk.Button("Exit")
         self.btnExit.connect("clicked", self.destroy)
         self.btnExit.set_tooltip_text("Close application")
-
+        
         self.btnRun = gtk.Button("Run")
         self.btnRun.connect("clicked", self.process)
+
+        self.btnFirmwares = gtk.Button("Firmwares")
+        self.btnFirmwares.connect("clicked", self.find_firmwares)
+
+        self.btnModules = gtk.Button("Modules")
+        self.btnModules.connect("clicked", self.find_modules)
 
         self.btnAbout = gtk.Button("About")
         self.btnAbout.connect("clicked", self.about)
 
         self.toolbar = gtk.HBox()
         self.toolbar.pack_start(self.btnRun)
+        self.toolbar.pack_start(self.btnFirmwares)
+        self.toolbar.pack_start(self.btnModules)
         self.toolbar.pack_start(self.btnAbout)
         self.toolbar.pack_start(self.btnExit)
+
+        self.box1.pack_start(self.toolbar)
+
+        ##### Tree
+        
+        self.load_airframe_xml()
+        self.fill_tree_from_airframe()
+        self.box1.pack_start(self.treeview)
+        
+        ##### Bottom        
 
         self.combo = gtk.combo_box_entry_new_text()
         self.combo.append_text("Entry 1")
@@ -93,10 +176,8 @@ class Base:
         self.label1 = gtk.Label("")
 
         self.textbox = gtk.Entry()
-        self.textbox.connect("changed", self.textchanged)
-
-        self.box1 = gtk.VBox()
-        self.box1.pack_start(self.toolbar)
+        self.textbox.connect("changed",self.textchanged)
+        
         self.box1.pack_start(self.label1)
         self.box1.pack_start(self.textbox)
         self.box1.pack_start(self.combo)
@@ -111,3 +192,4 @@ class Base:
 if __name__ == "__main__":
     base = Base()
     base.main()
+
